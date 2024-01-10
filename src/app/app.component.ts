6513b4813859ac9ba426cdd2cbbf96070ae6778d
DmitriyKovchugo <dkovchugo@mail.ru>
2024-01-10 17:57:29 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {ProductComponent} from "./components/product/product.component";
+import {products as data} from "./data/products";
+import {IProduct} from "./models/product";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-crash-course';
+
+  products: IProduct[] = data
 }
---
+++ src/app/models/product.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+  image: string;
+  price: number;
+  rating: { rate: number; count: number };
+  description: string;
+  id: number;
+  title: string;
+  category: string
+}
+
