014830ec732584f692c43f0f31b299c71b3bd951
DmitriyKovchugo <dkovchugo@mail.ru>
2024-01-10 19:51:43 +0300

    feat: add dynamic data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,31 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {ProductComponent} from "./components/product/product.component";
-import {products as data} from "./data/products";
 import {IProduct} from "./models/product";
+import {HttpClientModule} from "@angular/common/http";
+import {ProductsServices} from "./services/products.services";
+import {OnInit} from "@angular/core";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ProductComponent],
+  imports: [CommonModule, RouterOutlet, ProductComponent, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [ProductsServices]
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'angular-crash-course';
 
-  products: IProduct[] = data
+  products: IProduct[] = []
+
+  constructor(private productsService: ProductsServices) {
+  }
+
+  ngOnInit():void {
+    this.productsService.getAll().subscribe(products => {
+      this.products = products
+    })
+  }
 }
